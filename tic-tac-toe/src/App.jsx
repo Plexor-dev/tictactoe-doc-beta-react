2a30dca1f156bc8008031b0ba3b8b0a3cc2f34ea
Federico <llanofederico@gmail.com>
2023-02-28 18:58:45 -0300

    add linkks repo and beta react doc link

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
     <div className="App">
       <h1>Tic-tac-Toe</h1>
       <Board />
+      <h3><a href="https://beta.reactjs.org/learn/tutorial-tic-tac-toe">Tutorial tictactoe documentation beta react</a></h3>
+      <h3><a href="https://github.com/Plexor-dev/tictactoe-doc-beta-react">Git-hub Repository</a></h3>
     </div>
   )
 }
