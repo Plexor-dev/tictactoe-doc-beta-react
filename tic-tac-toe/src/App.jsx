3f5919df11881c494a9a5d4dd5780d528aa48c60
Federico <llanofederico@gmail.com>
2023-03-01 10:35:52 -0300

    add new target _blank

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -14,9 +14,9 @@ function App() {
       <button className='reset' onClick={()=> location.reload()}>Reset</button>
     </div>
     <div className='links'>
-      <h3><a href="https://beta.reactjs.org/learn/tutorial-tic-tac-toe">Tutorial tictactoe documentation beta React</a></h3>
-      <h3><a href="https://github.com/Plexor-dev/tictactoe-doc-beta-react">GitHub Repository</a></h3>
-      <p><span className='todo'>TODO: </span> better desing, reset button, history</p>
+      <h3><a href="https://beta.reactjs.org/learn/tutorial-tic-tac-toe" target="_blank">Tutorial tictactoe documentation beta React</a></h3>
+      <h3><a href="https://github.com/Plexor-dev/tictactoe-doc-beta-react" target="_blank">GitHub Repository</a></h3>
+      <p><span className='todo'>TODO: </span> better desing, history</p>
     </div>
     </>
   )
